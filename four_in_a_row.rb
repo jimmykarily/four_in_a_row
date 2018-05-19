6659ad4b659b8159f58662c48ace165114bcfc2d
Dimitris Karakasilis <jimmykarily@gmail.com>
2018-05-19 12:49:25 +0300

    Implemented initalize_board

--- four_in_a_row.rb
+++ four_in_a_row.rb
@@ -1,7 +1,12 @@
 #!/bin/env ruby
+require 'json'
 
 class FourInARow
-  def initialize
+  attr_reader :input_json, :parsed_json
+
+  def initialize(input_json)
+    @input_json = input_json
+    @parsed_json = JSON.parse(input_json)  
   end
 
   def run
@@ -10,5 +15,17 @@ class FourInARow
     calculate_valid_moves
     evaluate_move_scores # Initially random
     play_move
+  end
+  
+  def initialize_board
+    board = []
+    7.times { board << []; board }
+
+    parsed_json["moves"].each_with_index do |move, index|
+      value = index.odd? ? 2 : 1 # Zero based index
+      board[move - 1] = board[move - 1].to_a << value
+    end
+
+    board
   end
 end
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,28 +1,30 @@
 describe FourInARow do
-  let(:empty_board) do
-    board = []
-    7.times do |col|
-      board[col] = []
-      6.times do
-        board[col] << 0
-      end
-    end
+  let(:bot) { FourInARow.new(input) }
 
-    board.to_s
+  describe "#initialize" do
+    let(:input) do
+    '{ "type": "move", "moves": [1,1,3,2] }'
+    end 
+
+    it "parses the JSON" do
+      expect(bot.parsed_json["type"]).to eq("move")
+      expect(bot.parsed_json["moves"]).to eq([1,1,3,2])
+    end
   end
 
   describe "#initialize_board" do
     let(:input) do
-      { type: "move", moves: [1,1,3,2] }
+      '{ "type": "move", "moves": [1,1,3,2] }'
     end
-    let(:bot) { FourInARow.new(input) }
 
     let(:expected_board) do
-      board = empty_board
+      board = [[], [], [], [], [], [], []]
       board[0][0] = 1
       board[0][1] = 2
       board[2][0] = 1
       board[1][0] = 2
+
+      board
     end
 
     it "creates a board based on the input" do
