a4c9062ef136b7812c43da29787aeb9cc3af9593
Dimitris Karakasilis <jimmykarily@gmail.com>
2018-05-19 13:28:19 +0300

    Validate board

--- four_in_a_row.rb
+++ four_in_a_row.rb
@@ -2,15 +2,16 @@
 require 'json'
 
 class FourInARow
-  attr_reader :input_json, :parsed_json
+  attr_reader :input_json, :parsed_json, :board, :invalid_state
 
   def initialize(input_json)
     @input_json = input_json
     @parsed_json = JSON.parse(input_json)  
+
+    initialize_board
   end
 
   def run
-    initialize_board
     validate_moves
     calculate_valid_moves
     evaluate_move_scores # Initially random
@@ -18,14 +19,39 @@ class FourInARow
   end
   
   def initialize_board
-    board = []
-    7.times { board << []; board }
+    @board = []
+    7.times { @board << []; @board }
+
+    # If last player played an even move, then we play the odd moves so we
+    # are player 1.
+    our_player = parsed_json["moves"].length.even? ? 1 : 2
 
     parsed_json["moves"].each_with_index do |move, index|
-      value = index.odd? ? 2 : 1 # Zero based index
-      board[move - 1] = board[move - 1].to_a << value
+      current_player = index.odd? ? 2 : 1 # Zero based index
+
+      # Detect invalid move on non-existent column
+      if move < 0 || move > 7
+        @invalid_state = {
+          type: our_player == current_player ? "invalid_me" : "invalid_opponent",
+          moves: parsed_json["moves"][0..index]
+        }
+
+        return false
+      end
+
+      @board[move - 1] = @board[move - 1].to_a << current_player
+
+      # Detect invalid move on filled column
+      if @board[move - 1].length > 6
+        @invalid_state = {
+          type: our_player == current_player ? "invalid_me" : "invalid_opponent",
+          moves: parsed_json["moves"][0..index]
+        }
+
+        return false
+      end
     end
 
-    board
+    return true
   end
 end
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -28,7 +28,75 @@ describe FourInARow do
     end
 
     it "creates a board based on the input" do
-      expect(bot.initialize_board).to eq(expected_board)
+      expect(bot.board).to eq(expected_board)
+    end
+
+    context "when the board is invalid by us because of filled in column" do
+      let(:input) do
+        # 7th play on column 1 was invalid
+        '{ "type": "move", "moves": [1,1,1,1,1,1,1,1] }'
+      end
+
+      it "assigns the :invalid_board" do
+        expect(bot.invalid_state).to eq(
+          { type: "invalid_me", moves: [1,1,1,1,1,1,1] }
+        )
+
+        expect(bot.board).to eq(
+          [[1,2,1,2,1,2,1],[],[],[],[],[],[]]
+        )
+      end
+    end
+
+    context "when the board is invalid by us because of non-existent column" do
+      let(:input) do
+        # We played on non existent column 9
+        '{ "type": "move", "moves": [9,1] }'
+      end
+
+      it "assigns the :invalid_board" do
+        expect(bot.invalid_state).to eq(
+          { type: "invalid_me", moves: [9] }
+        )
+
+        expect(bot.board).to eq(
+          [[],[],[],[],[],[],[]]
+        )
+      end
+    end
+
+    context "when the board is invalid by the opponent" do
+      let(:input) do
+        # 7th play on column 1 was invalid
+        '{ "type": "move", "moves": [1,1,1,1,1,1,1] }'
+      end
+
+      it "assigns the :invalid_board" do
+        expect(bot.invalid_state).to eq(
+          { type: "invalid_opponent", moves: [1,1,1,1,1,1,1] }
+        )
+
+        expect(bot.board).to eq(
+          [[1,2,1,2,1,2,1],[],[],[],[],[],[]]
+        )
+      end
+    end
+
+    context "when the board is invalid by opponent because of non-existent column" do
+      let(:input) do
+        # Opponent played on non existent column 9
+        '{ "type": "move", "moves": [9] }'
+      end
+
+      it "assigns the :invalid_board" do
+        expect(bot.invalid_state).to eq(
+          { type: "invalid_opponent", moves: [9] }
+        )
+
+        expect(bot.board).to eq(
+          [[],[],[],[],[],[],[]]
+        )
+      end
     end
   end
 end
